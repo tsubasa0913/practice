0184c8b32497ffcc93b98559f132ccf4fe292e48
tsubasa0913 <b1024178@fun.ac.jp>
2025-02-24 03:06:32 +0900

    addadd

--- src/app/ui/TopBar.tsx
+++ src/app/ui/TopBar.tsx
@@ -8,7 +8,7 @@ const roots = [
 
 export default function TopBar() {
     return(
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="text-center">
           {
             roots.map((root) => {
                 return (
